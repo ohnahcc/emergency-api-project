063a3a3bd611c7824008ef1d740f69e5548f9bd9
ohnahcc <qkrcskgh1015@naver.com>
2025-01-27 10:48:11 +0900

    두번째DB 로컬

--- src/main/java/com/example/config/FlywayConfig.java
+++ src/main/java/com/example/config/FlywayConfig.java
@@ -12,9 +12,9 @@ public class FlywayConfig {
         // 첫 번째 데이터베이스 Flyway 설정
         return Flyway.configure()
                 .dataSource(
-                        System.getenv("SPRING_DATASOURCE_JDBC_URL"), // 첫 번째 DB URL
-                        System.getenv("SPRING_DATASOURCE_USERNAME"), // 첫 번째 DB 사용자
-                        System.getenv("SPRING_DATASOURCE_PASSWORD")  // 첫 번째 DB 비밀번호
+                        "jdbc:postgresql://postgres-db1:5432/emergency_api", // 첫 번째 DB URL
+                        "api_user", // 첫 번째 DB 사용자
+                        "1234"      // 첫 번째 DB 비밀번호
                 )
                 .locations("classpath:db/migration")
                 .baselineOnMigrate(true) // 기존 데이터베이스 초기화를 허용
@@ -22,29 +22,15 @@ public class FlywayConfig {
                 .load();
     }
 
-    @Bean
-    public Flyway secondaryFlyway() {
-        // 두 번째 데이터베이스 Flyway 설정
-        return Flyway.configure()
-                .dataSource(
-                        System.getenv("SPRING_SECOND_DATASOURCE_JDBC_URL"), // 두 번째 DB URL
-                        System.getenv("SPRING_SECOND_DATASOURCE_USERNAME"), // 두 번째 DB 사용자
-                        System.getenv("SPRING_SECOND_DATASOURCE_PASSWORD")  // 두 번째 DB 비밀번호
-                )
-                .locations("classpath:db/secondary_migration")
-                .baselineOnMigrate(true) // 기존 데이터베이스 초기화를 허용
-                .cleanDisabled(true)     // Flyway clean 방지
-                .load();
-    }
+
+
 
     @Bean
     public void migrateDatabases() {
-        // 두 Flyway Bean을 가져와 마이그레이션 수행
+        //  Flyway Bean을 가져와 마이그레이션 수행
         primaryFlyway().migrate();
         System.out.println("Primary database migration completed!");
 
-        secondaryFlyway().migrate();
-        System.out.println("Secondary database migration completed!");
     }
 }
 
